90b73a4d9436194aa468ac64075c1a7efa9805f9
Aleksei Gusev <aleksei.gusev@gmail.com>
2010-04-25 21:58:00 +0300

    refactoring: moved tests near to functions they test.

--- yari.el
+++ yari.el
@@ -72,12 +72,41 @@
     (define-key map (kbd "\C-?") 'scroll-down)
     map))
 
+(defmacro when-ert-loaded (&rest body)
+  `(when (featurep 'ert)
+     ,@body))
+
+(when-ert-loaded
+ (defmacro yari-with-ruby-obarray-cache-mock (cache-mock &rest body)
+   (declare (indent 1))
+   `(unwind-protect
+	(let* ((,cache-mock '("NotExistClassInRuby" "NotExistClassInRuby#mmmmm"))
+               (yari-ruby-obarray-cache ,cache-mock))
+          ,@body))))
+
+
 (defun yari-ri-lookup (name)
   "Return content from ri for NAME."
   (assert (member name (yari-ruby-obarray)) nil
           (format "%s is unknown symbol to RI." name))
   (shell-command-to-string
    (format "ri -T %s" (shell-quote-argument name))))
+
+(when-ert-loaded
+ (ert-deftest yari-test-ri-lookup-should-generate-error ()
+   (ert-should-error
+    (yari-ri-lookup "AbSoLuTttelyImposibleThisexists#bbb?")))
+
+ (ert-deftest yari-test-ri-lookup-should-have-content ()
+   (ert-should (string-match "Array < Object" (yari-ri-lookup "Array"))))
+
+ (ert-deftest yari-test-ri-lookup ()
+   (ert-should (yari-ri-lookup "Array")))
+
+ (ert-deftest yari-test-ruby-methods-from-ri-filter-standard-ruler ()
+   (ert-should-not (member "----------------------------------------------"
+                           (yari-ruby-methods-from-ri)))))
+
 
 (defvar yari-ruby-obarray-cache nil
   "Variable to store all possible completions of RI pages.")
@@ -88,13 +117,38 @@
       ;; TODO: I do not know how to return from here properly... ;]
       (setq yari-ruby-obarray-cache yari-ruby-obarray-cache)
     (let* ((methods (yari-ruby-methods-from-ri))
-           (classes '()))
-      (mapcar '(lambda (m)
-                 (setq classes (append classes
-                                       (yari-get-all-modules m))))
-              methods)
+           (classes (yari-ruby-classes-from-ri)))
       (setq yari-ruby-obarray-cache
             (delete-dups (append methods classes))))))
+
+(when-ert-loaded
+ (ert-deftest yari-test-ruby-obarray-should-rehash ()
+   (yari-with-ruby-obarray-cache-mock cache-mock
+     (yari-ruby-obarray t)
+     (ert-should-not (equal yari-ruby-obarray-cache cache-mock))))
+
+ (ert-deftest yari-test-ruby-obarray-should-use-cache ()
+   (yari-with-ruby-obarray-cache-mock cache-mock
+     (yari-ruby-obarray)
+     (ert-should (equal yari-ruby-obarray-cache cache-mock))))
+
+ (ert-deftest yari-test-ruby-obarray-should-set-cache ()
+   (let ((yari-ruby-obarray-cache))
+     (yari-ruby-obarray)
+     (ert-should yari-ruby-obarray-cache)))
+
+ (ert-deftest yari-test-ruby-obarray-for-class-first-level ()
+   (ert-should (member "RDoc" (yari-ruby-obarray))))
+
+ (ert-deftest yari-test-ruby-obarray-for-class-deep-level ()
+   (ert-should (member "RDoc::RI::Driver" (yari-ruby-obarray))))
+
+ (ert-deftest yari-test-ruby-obarray-for-class-method ()
+   (ert-should (member "RDoc::RI::Driver::new" (yari-ruby-obarray))))
+
+ (ert-deftest yari-test-ruby-obarray-for-object-method ()
+   (ert-should (member "RDoc::RI::Driver#parse_name" (yari-ruby-obarray)))))
+
 
 (defun yari-get-all-modules (name)
   "Return class or module names for RDoc::RI::Driver#run or
@@ -110,6 +164,19 @@
 		(match-string 1 module-name)
               "")))
     variants))
+
+(when-ert-loaded
+ (ert-deftest yari-test-get-all-modules ()
+   (ert-should (equal '("Array")
+                      (yari-get-all-modules "Array::new")))
+   (ert-should (equal '("Array")
+                      (yari-get-all-modules "Array#size")))
+   (ert-should (equal '("RDoc" "RDoc::RI" "RDoc::RI::Driver")
+                      (yari-get-all-modules "RDoc::RI::Driver")))
+   (ert-should (equal '("RDoc" "RDoc::RI" "RDoc::RI::Driver")
+                      (yari-get-all-modules "RDoc::RI::Driver#run")))
+   (ert-should (equal '("RDoc" "RDoc::RI" "RDoc::RI::Driver")
+                      (yari-get-all-modules "RDoc::RI::Driver::new")))))
 
 
 (defun yari-ruby-methods-from-ri ()
@@ -131,76 +198,31 @@
                       (split-string (shell-command-to-string "ri -T '.'")
                                     "\n"))))))
 
-;;; Tests:
+(when-ert-loaded
+ (ert-deftest yari-test-ruby-methods-from-ri-filter-standard-warning ()
+   (ert-should-not (member ". not found, maybe you meant:"
+                           (yari-ruby-methods-from-ri))))
 
-(when (featurep 'ert)
+ (ert-deftest yari-test-ruby-methods-from-ri-filter-empty-string ()
+   (ert-should-not (member "" (yari-ruby-methods-from-ri)))))
 
-  (ert-deftest yari-test-get-module-name ()
-    (ert-should (equal '("Array")
-                       (yari-get-all-modules "Array::new")))
-    (ert-should (equal '("Array")
-                       (yari-get-all-modules "Array#size")))
-    (ert-should (equal '("RDoc" "RDoc::RI" "RDoc::RI::Driver")
-                       (yari-get-all-modules "RDoc::RI::Driver")))
-    (ert-should (equal '("RDoc" "RDoc::RI" "RDoc::RI::Driver")
-                       (yari-get-all-modules "RDoc::RI::Driver#run")))
-    (ert-should (equal '("RDoc" "RDoc::RI" "RDoc::RI::Driver")
-                       (yari-get-all-modules "RDoc::RI::Driver::new"))))
 
-  (ert-deftest yari-test-ri-lookup-should-generate-error ()
-    (ert-should-error
-     (yari-ri-lookup "AbSoLuTttelyImposibleThisexists#bbb?")))
+(defun yari-ruby-classes-from-ri ()
+  "Return list with all ruby classes/modules know to ri command."
+  (let ((ruby-code "require 'rdoc/ri/driver';       \
+                    driver  = RDoc::RI::Driver.new; \
+                    classes = [];                   \
+                    driver.stores.each do |store|   \
+                      classes << store.modules;     \
+                    end;                            \
+                    puts classes.flatten.uniq"))
+    (split-string (shell-command-to-string
+                   (format "ruby -e\"%s\"" ruby-code)))))
 
-  (ert-deftest yari-test-ri-lookup-should-have-content ()
-    (ert-should (string-match "Array < Object" (yari-ri-lookup "Array"))))
-
-  (ert-deftest yari-test-ri-lookup ()
-    (ert-should (yari-ri-lookup "Array")))
-
-  (ert-deftest yari-test-ruby-methods-from-ri-filter-standard-ruler ()
-    (ert-should-not (member "----------------------------------------------"
-                            (yari-ruby-methods-from-ri))))
-
-  (ert-deftest yari-test-ruby-methods-from-ri-filter-standard-warning ()
-    (ert-should-not (member ". not found, maybe you meant:"
-                            (yari-ruby-methods-from-ri))))
-
-  (ert-deftest yari-test-ruby-methods-from-ri-filter-empty-string ()
-    (ert-should-not (member "" (yari-ruby-methods-from-ri))))
-
-  (ert-deftest yari-test-ruby-obarray-should-rehash ()
-    (yari-with-ruby-obarray-cache-mock cache-mock
-      (yari-ruby-obarray t)
-      (ert-should-not (equal yari-ruby-obarray-cache cache-mock))))
-
-  (ert-deftest yari-test-ruby-obarray-should-use-cache ()
-    (yari-with-ruby-obarray-cache-mock cache-mock
-      (yari-ruby-obarray)
-      (ert-should (equal yari-ruby-obarray-cache cache-mock))))
-
-  (ert-deftest yari-test-ruby-obarray-should-set-cache ()
-    (let ((yari-ruby-obarray-cache))
-      (yari-ruby-obarray)
-      (ert-should yari-ruby-obarray-cache)))
-
-  (ert-deftest yari-test-ruby-obarray-for-class-first-level ()
-    (ert-should (member "RDoc" (yari-ruby-obarray))))
-
-  (ert-deftest yari-test-ruby-obarray-for-class-deep-level ()
-    (ert-should (member "RDoc::RI::Driver" (yari-ruby-obarray))))
-
-  (ert-deftest yari-test-ruby-obarray-for-class-method ()
-    (ert-should (member "RDoc::RI::Driver::new" (yari-ruby-obarray))))
-
-  (ert-deftest yari-test-ruby-obarray-for-object-method ()
-    (ert-should (member "RDoc::RI::Driver#parse_name" (yari-ruby-obarray))))
-
-  (defmacro yari-with-ruby-obarray-cache-mock (cache-mock &rest body)
-    (declare (indent 1))
-    `(unwind-protect
-         (let* ((,cache-mock '("NotExistClassInRuby" "NotExistClassInRuby#mmmmm"))
-		(yari-ruby-obarray-cache ,cache-mock))
-           ,@body))))
+(when-ert-loaded
+ (ert-deftest yari-test-ruby-classes-from-ri ()
+   (yari-with-ruby-obarray-cache-mock cache-mock
+     (ert-should (member "RDoc" (yari-ruby-classes-from-ri))))))
 
 (provide 'yari)
 ;;; yari.el ends here
