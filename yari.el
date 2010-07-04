5fedbd740d5f233fd127f8a36c5f8a6910f0f4ca
Aleksei Gusev <aleksei.gusev@gmail.com>
2010-07-04 17:14:03 +0300

    added comment with example of using yari-anything.

--- yari.el
+++ yari.el
@@ -39,6 +39,12 @@
 ;;
 ;; (defun ri-bind-key ()
 ;;   (local-set-key [f1] 'yari))
+;;
+;;  or
+;;
+;; (defun ri-bind-key ()
+;;   (local-set-key [f1] 'yari-anything))
+;;
 ;; (add-hook 'ruby-mode-hook 'ri-bind-key)
 ;;
 ;; You can use C-u M-x yari to reload all completion targets.
